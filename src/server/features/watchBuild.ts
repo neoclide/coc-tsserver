898bf1585f718d5992687cc96d50eaf1046226b6
chemzqm <chemzqm@gmail.com>
2019-04-12 16:33:19 +0800

    use vim to run watchBuild

--- src/server/features/watchBuild.ts
+++ src/server/features/watchBuild.ts
@@ -1,5 +1,5 @@
 import { ChildProcess, spawn } from 'child_process'
-import { disposeAll, StatusBarItem, workspace } from 'coc.nvim'
+import { disposeAll, StatusBarItem, workspace, TaskOptions } from 'coc.nvim'
 import { Command, CommandManager } from 'coc.nvim/lib/commands'
 import findUp from 'find-up'
 import fs from 'fs'
@@ -9,6 +9,7 @@ import { Disposable, Location } from 'vscode-languageserver-protocol'
 import Uri from 'vscode-uri'
 import which from 'which'
 import { resolveRoot } from '../utils/fs'
+import Task from 'coc.nvim/lib/model/task'
 
 const TSC = './node_modules/.bin/tsc'
 const countRegex = /Found\s+(\d+)\s+error/
@@ -27,20 +28,64 @@ enum TscStatus {
   ERROR,
 }
 
-class WatchCommand implements Command {
-  public readonly id: string = 'tsserver.watchBuild'
+export default class WatchProject implements Disposable {
+  private disposables: Disposable[] = []
+  public static readonly id: string = 'tsserver.watchBuild'
+  public static readonly startTexts: string[] = ['Starting compilation in watch mode', 'Starting incremental compilation']
   private statusItem: StatusBarItem
   private isRunning = false
-  private process: ChildProcess
+  private task: Task
+  private options: TaskOptions
 
-  constructor() {
+  public constructor(
+    commandManager: CommandManager
+  ) {
     this.statusItem = workspace.createStatusBarItem(1, { progress: true })
+    let task = this.task = workspace.createTask('TSC')
+    this.options = this.getOptions()
+    this.disposables.push(commandManager.registerCommand(WatchProject.id, async () => {
+      await this.start(this.options)
+    }))
+    task.onExit(code => {
+      if (code != 0) {
+        workspace.showMessage(`TSC exit with code ${code}`, 'warning')
+      }
+      this.onStop()
+    })
+    task.onStdout(lines => {
+      for (let line of lines) {
+        this.onLine(line)
+      }
+    })
+    task.onStderr(lines => {
+      workspace.showMessage(`TSC error: ` + lines.join('\n'), 'error')
+    })
+    this.disposables.push(Disposable.create(() => {
+      task.dispose()
+    }))
+    this.check().catch(_e => {
+      // noop
+    })
+  }
+
+  private async check(): Promise<void> {
+    let running = await this.task.running
+    if (running) {
+      this.statusItem.isProgress = false
+      this.statusItem.text = '?'
+      this.statusItem.show()
+    } else {
+      this.onStop()
+    }
+  }
+
+  private async start(options: TaskOptions): Promise<void> {
+    await this.task.start(options)
   }
 
   private onStop(): void {
     let { nvim } = workspace
     this.isRunning = false
-    nvim.setVar('Tsc_running', 0, true)
     this.statusItem.hide()
   }
 
@@ -51,60 +96,43 @@ class WatchCommand implements Command {
     workspace.nvim.call('setqflist', [[], 'r'], true)
   }
 
-  private async start(cmd: string, args: string[], cwd: string): Promise<void> {
-    if (this.isRunning) {
-      this.process.kill()
-      await wait(200)
+  private onLine(line: string): void {
+    if (countRegex.test(line)) {
+      let ms = line.match(countRegex)
+      this.statusItem.text = ms[1] == '0' ? '✓' : '✗'
+      this.statusItem.isProgress = false
+    } else if (WatchProject.startTexts.findIndex(s => line.indexOf(s) !== -1) != -1) {
+      this.onStart()
+    } else {
+      let ms = line.match(errorRegex)
+      if (!ms) return
+      let fullpath = path.join(this.options.cwd, ms[1])
+      let uri = Uri.file(fullpath).toString()
+      let doc = workspace.getDocument(uri)
+      let bufnr = doc ? doc.bufnr : null
+      let item = {
+        filename: fullpath,
+        lnum: Number(ms[2]),
+        col: Number(ms[3]),
+        text: `[tsc ${ms[5]}] ${ms[6]}`,
+        type: /error/i.test(ms[4]) ? 'E' : 'W'
+      } as any
+      if (bufnr) item.bufnr = bufnr
+      workspace.nvim.call('setqflist', [[item], 'a'])
     }
-    this.isRunning = true
-    workspace.nvim.setVar('Tsc_running', 1, true)
-    this.process = spawn(cmd, args, { cwd })
-    this.process.on('error', e => {
-      workspace.showMessage(e.message, 'error')
-    })
-    const rl = readline.createInterface(this.process.stdout)
-    this.process.on('exit', () => {
-      this.onStop()
-      rl.close()
-    })
-    this.process.stderr.on('data', chunk => {
-      workspace.showMessage(chunk.toString('utf8'), 'error')
-    })
-    const startTexts = ['Starting compilation in watch mode', 'Starting incremental compilation']
-    rl.on('line', line => {
-      if (countRegex.test(line)) {
-        let ms = line.match(countRegex)
-        this.statusItem.text = ms[1] == '0' ? '✓' : '✗'
-        this.statusItem.isProgress = false
-      } else if (startTexts.findIndex(s => line.indexOf(s) !== -1) != -1) {
-        this.onStart()
-      } else {
-        let ms = line.match(errorRegex)
-        if (!ms) return
-        let fullpath = path.join(cwd, ms[1])
-        let uri = Uri.file(fullpath).toString()
-        let doc = workspace.getDocument(uri)
-        let bufnr = doc ? doc.bufnr : null
-        let item = {
-          filename: fullpath,
-          lnum: Number(ms[2]),
-          col: Number(ms[3]),
-          text: `[tsc ${ms[5]}] ${ms[6]}`,
-          type: /error/i.test(ms[4]) ? 'E' : 'W'
-        } as any
-        if (bufnr) item.bufnr = bufnr
-        workspace.nvim.call('setqflist', [[item], 'a'])
-      }
-    })
   }
 
-  public async execute(): Promise<void> {
+  public getOptions(): TaskOptions {
     let docs = workspace.documents
     let idx = docs.findIndex(doc => doc.uri.indexOf(TSC) !== -1)
     if (idx !== -1) return
-    let document = await workspace.document
-    let fsPath = Uri.parse(document.uri).fsPath
-    let cwd = path.dirname(fsPath)
+    let doc = workspace.getDocument(workspace.bufnr)
+    let cwd: string
+    if (doc && doc.schema == 'file') {
+      cwd = path.dirname(Uri.parse(doc.uri).fsPath)
+    } else {
+      cwd = workspace.cwd
+    }
     let res = findUp.sync(['node_modules'], { cwd })
     let cmd: string
     let root: string
@@ -130,26 +158,11 @@ class WatchCommand implements Command {
       return
     }
     let configPath = path.relative(root, path.join(configRoot, 'tsconfig.json'))
-    this.start(cmd, ['-p', configPath, '--watch', 'true', '--pretty', 'false'], root)
-  }
-}
-
-export default class WatchProject implements Disposable {
-  private disposables: Disposable[] = []
-
-  public constructor(
-    commandManager: CommandManager
-  ) {
-    let cmd = new WatchCommand()
-    commandManager.register(cmd)
-    let { nvim } = workspace
-    nvim.getVar('Tsc_running').then(running => {
-      if (running) {
-        cmd.execute().catch(e => {
-          workspace.showMessage('TSC:' + e.message, 'error')
-        })
-      }
-    })
+    return {
+      cmd,
+      args: ['-p', configPath, '--watch', 'true', '--pretty', 'false'],
+      cwd: root
+    }
   }
 
   public dispose(): void {
